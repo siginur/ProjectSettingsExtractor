1b013d8b234089bceb3ad822c08fa31a13be0062
Alexey Siginur <siginur@gmail.com>
2019-01-30 21:26:03 +0200

    Initial Commit

---
+++ ProjectSettingsExtractor/ProjectSettingsExtractor.h
@@ -0,0 +1,19 @@
+//
+//  ProjectSettingsExtractor.h
+//  ProjectSettingsExtractor
+//
+//  Created by Alexey Siginur on 30/01/2019.
+//  Copyright © 2019 merkova. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProjectSettingsExtractor.
+FOUNDATION_EXPORT double ProjectSettingsExtractorVersionNumber;
+
+//! Project version string for ProjectSettingsExtractor.
+FOUNDATION_EXPORT const unsigned char ProjectSettingsExtractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectSettingsExtractor/PublicHeader.h>
+
+
